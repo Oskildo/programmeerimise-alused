7a82f72ea84a1e522e54513fb6a44ed069b78b5b
Lord-Mike-y <83661333+Lord-Mike-y@users.noreply.github.com>
2021-05-11 20:33:46 +0300

    Update Juhuslik_fakt.py

--- 01 - rakendusliidesed/Juhuslik_fakt.py
+++ 01 - rakendusliidesed/Juhuslik_fakt.py
@@ -4,9 +4,17 @@ aadress = "https://cat-fact.herokuapp.com/facts/random"
 päring = requests.get(aadress)
 andmed = päring.json()
 valikud =["jah", "ei"]
-answer = buttonbox("Would you like to know something interesting?", choices = valikud)
-if answer == "jah":
-    msgbox(andmed['text'])
-else:
-    msgbox("Kurb lugu.")
-    msgbox(andmed['text'])
+kordus = 1
+i = 0
+
+while i < kordus:
+    answer = buttonbox("Would you like to know something interesting?", choices = valikud)
+    if answer == "jah":
+        msgbox(andmed['text'])
+        i = i + 1
+        kordus = kordus + 1
+    else:
+        msgbox("Kurb lugu.")
+        msgbox(andmed['text'])
+        i = i + 1
+        kordus = kordus - 1 
