b3000ef0711da824fbb70b5b40d4b950ad979205
Skullyboy <martinvadi01@gmail.com>
2021-05-11 22:36:06 +0300

    Update Juhuslik_fakt.py

--- 01 - rakendusliidesed/Juhuslik_fakt.py
+++ 01 - rakendusliidesed/Juhuslik_fakt.py
@@ -4,17 +4,30 @@ aadress = "https://cat-fact.herokuapp.com/facts/random"
 päring = requests.get(aadress)
 andmed = päring.json()
 valikud =["jah", "ei"]
+valik = ["jah, tahan veel ühte","ei, aitab küll"]
 kordus = 1
 i = 0
 
 while i < kordus:
     answer = buttonbox("Would you like to know something interesting?", choices = valikud)
     if answer == "jah":
+        päring = requests.get(aadress)
+        andmed = päring.json()
         msgbox(andmed['text'])
         i = i + 1
         kordus = kordus + 1
+        answer = buttonbox("Tahad veel ühte fakti?", choices = valik)
+        if answer == "jah, tahan veel ühte":
+            päring = requests.get(aadress)
+            andmed = päring.json()
+            msgbox(andmed['text'])
+            i = i + 1
+            kordus = kordus + 1
+                
+        else:
+            msgbox("Kena päeva!")
+            break
     else:
         msgbox("Kurb lugu.")
         msgbox(andmed['text'])
-        i = i + 1
-        kordus = kordus - 1 
+        break
