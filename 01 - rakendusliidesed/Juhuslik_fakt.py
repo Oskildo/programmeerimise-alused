b455a2c96f17bb11b21d56162b16fd81028388db
Oskildo <83445814+Oskildo@users.noreply.github.com>
2021-05-11 20:25:59 +0300

    Update Juhuslik_fakt.py

--- 01 - rakendusliidesed/Juhuslik_fakt.py
+++ 01 - rakendusliidesed/Juhuslik_fakt.py
@@ -1,5 +1,12 @@
 import requests
+from easygui import *  
 aadress = "https://cat-fact.herokuapp.com/facts/random" 
 päring = requests.get(aadress)
 andmed = päring.json()
-print(andmed['text'])
+valikud =["jah", "ei"]
+answer = buttonbox("Would you like to know something interesting?", choices = valikud)
+if answer == "jah":
+    msgbox(andmed['text'])
+else:
+    msgbox("Kurb lugu.")
+    msgbox(andmed['text'])
